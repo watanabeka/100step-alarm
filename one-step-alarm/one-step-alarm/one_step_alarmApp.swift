e159fac724ebbde10eaacbc659d2389dd69f9c8b
k-watanabe <wtnbkisi@gmail.com>
2025-12-31 09:07:38 +0900

    初回

---
+++ one-step-alarm/one-step-alarm/one_step_alarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  one_step_alarmApp.swift
+//  one-step-alarm
+//
+//  Created by 渡辺海星 on 2025/12/31.
+//
+
+import SwiftUI
+
+@main
+struct one_step_alarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
